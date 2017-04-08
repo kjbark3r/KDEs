883664120db1a87d079a0a50a81222a22f15a4b0
Kristin Barker <kristin.barker@umconnect.umt.edu>
2017-04-08 16:59:32 -0600

    Possibly updated for other general KDE creations? Can't remember

--- hunterKDEs.R
+++ hunterKDEs.R
@@ -32,14 +32,94 @@ rm(wd_workcomp, wd_laptop)
   latlong = CRS("+init=epsg:4326")
   
 ##COLLAR DATA
-elk <- read.csv("collardata-locsonly-equalsampling.csv", as.is = TRUE, header = TRUE)
+elk <- read.csv("../ElkDatabase/collardata-locsonly-equalsampling.csv", as.is = TRUE, header = TRUE)
 elk$Date <- as.Date(elk$Date, "%Y-%m-%d")
 
 ##SEASONAL KDES BY SEX
 
-indiv <- subset(elk, AnimalID == 151580)
+# AnimalID by DeviceID
+unique(elk[elk$DeviceID == 34918, 1])
+
+indiv <- subset(elk, AnimalID == 140630)
 xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
 ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
 kde <- kernelUD(ll, h="href", grid = 5000)
 raster <- raster(kde)
-writeRaster(raster, paste("KDE151580"), format="GTiff", overwrite=TRUE)
+writeRaster(raster, paste("KDE140630"), format="GTiff", overwrite=TRUE)
+
+
+##SEASONAL KDES FOR PRESENTATION EXAMPLE
+
+
+## resident: 140630-15 ##
+
+#winter
+indiv <- elk %>%
+  subset(AnimalID == 140630) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-02-15"), as.Date("2015-03-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE140630-15win"), format="GTiff", overwrite=TRUE)
+#summer
+indiv <- elk %>%
+  subset(AnimalID == 140630) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-07-15"), as.Date("2015-08-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE140630-15sum"), format="GTiff", overwrite=TRUE)
+
+
+
+## migrant: 141150-15 ##
+
+#winter
+indiv <- elk %>%
+  subset(AnimalID == 141150) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-02-15"), as.Date("2015-03-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE141150-15win"), format="GTiff", overwrite=TRUE)
+#summer
+indiv <- elk %>%
+  subset(AnimalID == 141150) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-07-15"), as.Date("2015-08-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE141150-15sum"), format="GTiff", overwrite=TRUE)
+
+
+
+## intermediate: 141490-15 ##
+
+#winter
+indiv <- elk %>%
+  subset(AnimalID == 141490) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-02-15"), as.Date("2015-03-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE141490-15win"), format="GTiff", overwrite=TRUE)
+#summer
+indiv <- elk %>%
+  subset(AnimalID == 141490) %>%
+  within(Date <- as.Date(Date, format = "%Y-%m-%d")) %>% 
+    subset(between(Date, as.Date("2015-07-15"), as.Date("2015-08-31"))) 
+xy <- data.frame("x"=indiv$Long,"y"=indiv$Lat)
+ll <- SpatialPointsDataFrame(xy, indiv, proj4string = latlong)
+kde <- kernelUD(ll, h="href", grid = 5000)
+raster <- raster(kde)
+writeRaster(raster, paste("KDE141490-15sum"), format="GTiff", overwrite=TRUE)
